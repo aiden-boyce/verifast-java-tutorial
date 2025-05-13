a8b0cce7ce071b0b0b1ca10288f62bf2a10ccd94
Aiden <aidentboyce@gmail.com>
2025-05-13 17:23:23 -0400

    Added "\" before "@" to prevent VeriFast from reading functions not complete yet.

--- Max.java
+++ Max.java
@@ -1,4 +1,8 @@
 /**************************************************  
+ * Remove "\" before "@"
+ * Make sure there is no space between the comment 
+ * tag and "@" symbol.
+--------------------------------------------------- 
 Part 1:    
     Define the Preconditions and Postconditions
     - Preconditions: 
@@ -13,25 +17,25 @@ Part 2:
 
 public class Max {
     public int max(int x, int y)
-    //@ requires ...
-    //@ ensures ...
+    //\@ requires ...
+    //\@ ensures ...
     {
         int max;
         if(x <= y) 
             max = 0;
         else 
             max = x;
-        //@ assert ...
+        //\@ assert ...
         return max;
     }
 
     public int max3(int x, int y, int z)
-    //@ requires ...
-    //@ ensures ...
+    //\@ requires ...
+    //\@ ensures ...
     {
         int max = max(x, y);
         max = max(max, z);
-        //@ assert ...
+        //\@ assert ...
         return max;
     }
 }
